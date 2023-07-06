b3b8e21e1f0b44447a2a2757cbd92b19b4db2bda
oda91-7 <yrhr.ldh.11@icloud.com>
2023-07-06 17:08:07 +0900

    a_complete

---
+++ a5.py
@@ -0,0 +1,5 @@
+Bob_info = ["Bob", "Dylan", 79]
+
+output = f"Name: {Bob_info[0]} {Bob_info[1]}, Age: {Bob_info[2]}"
+
+print(output)
---
+++ a7.py
@@ -0,0 +1,5 @@
+even_numbers = [2, 4, 6, 8]
+
+for number in even_numbers:
+    answer = number * 2
+    print(answer)
